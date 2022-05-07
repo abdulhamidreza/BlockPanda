c8ae7af926a056092ebc2c67931186956fa578cc
abdulhamid <abdul.hamid@paycraftsol.com>
2022-05-08 02:24:25 +0530

    UI data set

--- app/src/main/java/com/example/blockpanda/UsagesActivity.kt
+++ app/src/main/java/com/example/blockpanda/UsagesActivity.kt
@@ -26,12 +26,10 @@ class UsagesActivity : AppCompatActivity() {
     // Constants defining order for display order
     private val _DISPLAY_ORDER_USAGE_TIME = 0
     private val _DISPLAY_ORDER_LAST_TIME_USED = 1
-    private val _DISPLAY_ORDER_APP_NAME = 2
 
     private var mDisplayOrder = _DISPLAY_ORDER_USAGE_TIME
     private val mLastTimeUsedComparator = LastTimeUsedComparator()
     private val mUsageTimeComparator = UsageTimeComparator()
-    private var mAppLabelComparator: AppNameComparator? = null
     private val mAppLabelMap = ArrayMap<String, String>()
     private val mPackageStats = ArrayList<UsageStats>()
     var contacts: ArrayList<AppDetails>? = null
@@ -51,10 +49,6 @@ class UsagesActivity : AppCompatActivity() {
         mUsageStatsManager = getSystemService(USAGE_STATS_SERVICE) as UsageStatsManager
         mInflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater
         mPm = packageManager
-
-
-        /* val typeSpinner = findViewById<View>(R.id.typeSpinner) as Spinner
-         typeSpinner.setOnItemSelectedListener(this)*/
 
         // getting the recyclerview by its id
         val recyclerview = findViewById<RecyclerView>(R.id.rvContacts)
@@ -102,19 +96,8 @@ class UsagesActivity : AppCompatActivity() {
         }
         mPackageStats.addAll(map.values)
         // Sort list
-        mAppLabelComparator = AppNameComparator(mAppLabelMap)
-        sortList()
-
+        sortList(_DISPLAY_ORDER_USAGE_TIME)
         return mPackageStats
-    }
-
-
-    class AppNameComparator internal constructor(private val mAppLabelList: Map<String, String>) : Comparator<UsageStats> {
-        override fun compare(a: UsageStats, b: UsageStats): Int {
-            val alabel = mAppLabelList[a.packageName]
-            val blabel = mAppLabelList[b.packageName]
-            return alabel!!.compareTo(blabel!!)
-        }
     }
 
     class LastTimeUsedComparator : Comparator<UsageStats> {
@@ -131,25 +114,13 @@ class UsagesActivity : AppCompatActivity() {
     }
 
 
-    private fun sortList(sortOrder: Int) {
-        if (mDisplayOrder == sortOrder) {
-            // do nothing
-            return
-        }
-        mDisplayOrder = sortOrder
-        sortList()
-    }
-
-    private fun sortList() {
-        if (mDisplayOrder == _DISPLAY_ORDER_USAGE_TIME) {
+    private fun sortList(mDisplayOrder: Int) {
+        if (mDisplayOrder == this._DISPLAY_ORDER_USAGE_TIME) {
             if (localLOGV) Log.i(TAG, "Sorting by usage time")
             Collections.sort(mPackageStats, mUsageTimeComparator)
         } else if (mDisplayOrder == _DISPLAY_ORDER_LAST_TIME_USED) {
             if (localLOGV) Log.i(TAG, "Sorting by last time used")
             Collections.sort(mPackageStats, mLastTimeUsedComparator)
-        } else if (mDisplayOrder == _DISPLAY_ORDER_APP_NAME) {
-            if (localLOGV) Log.i(TAG, "Sorting by application name")
-            Collections.sort(mPackageStats, mAppLabelComparator)
         }
     }
 
